2f2ae529a449b2c7cdb90787614e0f7bdafd62f4
dlguswo333 <dlguswo333@skku.edu>
2021-07-01 16:38:39 +0900

    Redefine STATE

--- src/defs.js
+++ src/defs.js
@@ -2,25 +2,36 @@ const PORT = 9238;
 const CHUNKSIZE = 2097152;
 const HEADER_END = '\n\n';
 const STATE = {
+  // For Server, Client, Sender and Receiver
   ERR_FILE_SYSTEM: "ERR_FILE_SYSTEM",
   ERR_NETWORK: "ERR_NETWORK",
   ERR_ID: "ERR_ID",
   ERR_IP: "ERR_IP",
+
+  // For Server and Client
   INITING: "INITING",
   IDLE: "IDLE",
 
+  // For Receiver and Sender
+  MY_PAUSE: "MY_PAUSE",
+  OTHER_PAUSE: "OTHER_PAUSE",
+  MY_END: "MY_END",
+  OTHER_END: "OTHER_END",
+
+  // For Sender
   SEND_WAIT: "SEND_WAIT",
-  SEND: "SEND",
+  SENDING: "SEND",
   SEND_REJECT: "SEND_REJECT",
   SEND_COMPLETE: "SEND_COMPLETE",
-  SENDER_PAUSE: "SENDER_PAUSE",
-  SENDER_END: "SENDER_END",
+  SENDER_PAUSE: "SENDER_PAUSE", //Will be deprecated
+  SENDER_END: "SENDER_END", //Will be deprecated
 
+  // For Receiver
   RECV_WAIT: "RECV_WAIT",
-  RECV: "RECV",
+  RECVING: "RECVING",
   RECV_COMPLETE: "RECV_COMPLETE",
-  RECVER_PAUSE: "RECVER_PAUSE",
-  RECVER_END: "RECVER_END"
+  RECVER_PAUSE: "RECVER_PAUSE", //Will be deprecated
+  RECVER_END: "RECVER_END" //Will be deprecated
 };
 const OS = require('os').platform();
 const { version: VERSION } = require('../package.json');
