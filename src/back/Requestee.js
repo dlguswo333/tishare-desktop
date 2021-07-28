93d3fc9af11e1bc5fb974ed5741019ac1289382c
dlguswo333 <dlguswo333@skku.edu>
2021-07-28 13:40:19 +0900

    Fix showing error message after rejecting

--- src/back/Requestee.js
+++ src/back/Requestee.js
@@ -23,6 +23,7 @@ class Requestee {
    * Reject Request.
    */
   reject() {
+    this._haveRejectedFlag = true;
     if (this._state === STATE.RQE_SEND_REQUEST) {
       this.setState(STATE.RQE_SEND_REJECT);
       this._socket.write(JSON.stringify({ class: 'no' }) + HEADER_END, 'utf-8', this._onWriteError);
