6eb1c7cb7c6546b9c41f26c32128dd1812fd8c0b
dlguswo333 <dlguswo002@gmail.com>
2025-08-25 23:20:13 +0900

    [#93] Rewrite DeviceView in typescript

--- src/front/component/DeviceView.tsx
+++ src/front/component/DeviceView.tsx
@@ -4,25 +4,27 @@ import Device from './Device';
 import Spinner from '@dlguswo333/react-simple-spinner';
 import {SCANTIMEOUT} from '../../defs';
 import '../style/DeviceView.scss';
+import {TiDevice, TiItem} from '../../types';
 const ipcRenderer = window.ipcRenderer;
 
-/**
- *
- * @param {Object} props
- * @param {Object} props.items
- * @param {string | null} props.myIp
- * @param {string | null} props.myNetmask
- * @param {string} props.myId
- * @returns
- */
-function DeviceView ({items, myIp, myNetmask, myId}) {
-  const [devices, setDevices] = useState({});
+type Props = {
+  items: Record<string, TiItem>;
+  myIp: null | string;
+  myNetmask: null | string;
+  myId: string;
+}
+
+function DeviceView ({items, myIp, myNetmask, myId}: Props) {
+  const [devices, setDevices] = useState<Record<string, TiDevice>>({});
   // const [devices, setDevices] = useState({ '10.221.151.210': { ip: '10.221.151.210', id: 'linuxmachine', os: 'linux', version: '0.2.0' }, '10.221.151.200': { ip: '10.221.151.200', id: 'my phone', os: 'android', version: '0.2.0' } });
   const [selectedIp, setSelectedIp] = useState(null);
   const [noDeviceWarn, setNoDeviceWarn] = useState(false);
   const [scanning, setScanning] = useState(false);
 
   const scan = () => {
+    if (!(myIp && myNetmask)) {
+      return;
+    }
     setScanning(true);
     setTimeout(() => { setScanning(false); }, SCANTIMEOUT);
     setSelectedIp(null);
@@ -50,7 +52,7 @@ function DeviceView ({items, myIp, myNetmask, myId}) {
 
   useEffect(() => {
     ipcRenderer.scanCallback((deviceIp, deviceVersion, deviceId, deviceOs) => {
-      let tmp = {};
+      let tmp: Record<string, TiDevice> = {};
       tmp[deviceIp] = {ip: deviceIp, version: deviceVersion, id: deviceId, os: deviceOs};
       /**
        * NOTE
